4024e26942bde01e0e1b9a0b5b055ef2fe344b4c
Stefan Eletzhofer <se@nexiles.de>
2015-01-12 21:50:56 +0100

    Added scaffolding output for nexiles theme.
    Signed-off-by: Stefan Eletzhofer <se@nexiles.de>

---
+++ addons/nexiles_theme/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    # Theme information
+    'name': "nexiles theme",
+    'description': """A theme for the nexiles odoo setup.""",
+    'category': 'Theme',
+    'version': '1.0',
+    'depends': ['website'],
+
+    # templates, pages, and snippets
+    'data': [
+        'views/pages.xml',
+        'views/options.xml',
+        'views/snippets.xml',
+    ],
+
+    # Your information
+    'author': "nexiles GmbH",
+    'website': "http://nexiles.com",
+}
